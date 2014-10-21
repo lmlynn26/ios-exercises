710755e4846959349ac335d1a88abdb20317e706
Larry Lynn <llynn@takata.com>
2014-10-21 15:48:52 -0400

    Completed numbers and variables

--- BlocExercises/BlocExercises/SimpleCalculator.m
+++ BlocExercises/BlocExercises/SimpleCalculator.m
@@ -8,21 +8,41 @@
 
 #import "SimpleCalculator.h"
 
+
 @implementation SimpleCalculator
+
 
 - (NSInteger) increaseNumberBy1:(NSInteger) number {
     /* WORK HERE */
+    
+        number++;
+    
     return number;
+    
+    
 }
 
 - (NSInteger) addNumber:(NSInteger) number1 toNumber:(NSInteger) number2 {
     /* WORK HERE */
+    
+
+    number1 = number1 + number2;
+    
+    
+    
     return number1;
 }
 
 - (NSInteger) remainderOfNumber:(NSInteger) dividend dividedByNumber:(NSInteger) divisor {
     /* WORK HERE */
-    return 0;
+    
+    NSInteger result = 0;
+    
+    result = dividend % divisor;
+    
+
+    
+    return result;
 }
 
 @end
