ac0cc05b10233c1d0ff0d19ea621b7b7afe79524
Larry Lynn <llynn@takata.com>
2014-11-25 12:57:24 -0500

    Completed simple data types and objects

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -12,17 +12,79 @@
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
     /* WORK HERE */
-    return @0;
+    
+    
+    NSNumber *numberDouble = [NSNumber numberWithInt:([number doubleValue] * 2)];
+    
+    
+    
+    
+    return numberDouble;
+    
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @[];
+
+    
+    NSMutableArray *numbersArray = [[NSMutableArray alloc] init];
+    
+    if (number > otherNumber) {
+        NSInteger temp = number;
+        number = otherNumber;
+        otherNumber = temp;
+        
+    }
+    for (NSInteger i = number; i <= otherNumber; i++) {
+            [numbersArray addObject:@(i)];
+    }
+        
+    return numbersArray;
+
+    
+    //return @[];
+        
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
     /* WORK HERE */
-    return 0;
-}
+    
+//    NSMutableArray *numbersArray = [[NSMutableArray alloc] init];
+//    
+//    if (number > otherNumber) {
+//        NSInteger temp = number;
+//        number = otherNumber;
+//        otherNumber = temp;
+//        
+//    }
+//    for (NSInteger i = number; i <= otherNumber; i++) {
+//        [numbersArray addObject:@(i)];
+//    }
+//    
+//    return numbersArray;
+    
+    //[arrayOfNumbers sortedArrayUsingSelector:@selector(compare:)];
+    NSMutableArray *sortedArray = [arrayOfNumbers mutableCopy];
+    [sortedArray sortUsingSelector:@selector(compare:)];
+    
+    
+    NSNumber *number = sortedArray[0];
+    
+    return number.intValue;
+    
+//        NSNumber *int1 = (NSNumber *) obj1;
+//        NSNumber *int2 = (NSNumber *) obj2;
+//        return [int1 compare:int2];
+//    }];
+    
+    //return sortedArray[0];
+    
+    
+    
+    
+    //return [arrayOfNumbers [0];
 
+    //return 0;
+
+}
+    
 @end
