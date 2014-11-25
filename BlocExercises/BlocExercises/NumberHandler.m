49d20a3c8e325a76c8ebb190c9ce2e1365fdbea9
Larry Lynn <llynn@takata.com>
2014-11-25 12:59:51 -0500

    Completed Simple Data Types and Objects

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -17,8 +17,6 @@
     NSNumber *numberDouble = [NSNumber numberWithInt:([number doubleValue] * 2)];
     
     
-    
-    
     return numberDouble;
     
 }
@@ -40,29 +38,13 @@
         
     return numbersArray;
 
-    
-    //return @[];
         
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
     /* WORK HERE */
     
-//    NSMutableArray *numbersArray = [[NSMutableArray alloc] init];
-//    
-//    if (number > otherNumber) {
-//        NSInteger temp = number;
-//        number = otherNumber;
-//        otherNumber = temp;
-//        
-//    }
-//    for (NSInteger i = number; i <= otherNumber; i++) {
-//        [numbersArray addObject:@(i)];
-//    }
-//    
-//    return numbersArray;
-    
-    //[arrayOfNumbers sortedArrayUsingSelector:@selector(compare:)];
+
     NSMutableArray *sortedArray = [arrayOfNumbers mutableCopy];
     [sortedArray sortUsingSelector:@selector(compare:)];
     
@@ -71,20 +53,6 @@
     
     return number.intValue;
     
-//        NSNumber *int1 = (NSNumber *) obj1;
-//        NSNumber *int2 = (NSNumber *) obj2;
-//        return [int1 compare:int2];
-//    }];
-    
-    //return sortedArray[0];
-    
-    
-    
-    
-    //return [arrayOfNumbers [0];
-
-    //return 0;
-
 }
     
 @end
